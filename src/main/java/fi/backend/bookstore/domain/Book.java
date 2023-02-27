b98df2a63a117977e51d9f2305a2e3174e38dda6
attemkinen <atte.makinen1@outlook.com>
2023-02-27 11:34:01 +0200

    added json properties to bookcontroller

--- src/main/java/fi/backend/bookstore/domain/Book.java
+++ src/main/java/fi/backend/bookstore/domain/Book.java
@@ -1,5 +1,7 @@
 package fi.backend.bookstore.domain;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -17,6 +19,7 @@ public class Book {
 	private int published;
 	
 	@ManyToOne
+	@JsonIgnoreProperties ("booksjson")
 	@JoinColumn ( name = "categoryid")
 	private Category category;
 	
--- src/main/java/fi/backend/bookstore/web/BookController.java
+++ src/main/java/fi/backend/bookstore/web/BookController.java
@@ -1,33 +1,33 @@
 package fi.backend.bookstore.web;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import fi.backend.bookstore.domain.Book;
 import fi.backend.bookstore.domain.BookRepository;
 import fi.backend.bookstore.domain.CategoryRepository;
 
-
-
+@CrossOrigin
 @Controller
 public class BookController {
 	@Autowired
 	private BookRepository repository;
-	
+
 	@Autowired
 	private CategoryRepository crepository;
-	
-
-	
 
 	// http://localhost:8080/books
+	// return all the books in bookstore
 	@RequestMapping(value = "/books", method = RequestMethod.GET)
 	public String getBooks(Model model) {
 		List<Book> books = (List<Book>) repository.findAll();
@@ -36,6 +36,19 @@ public class BookController {
 
 	}
 
+	// return all existing books in JSON
+	@RequestMapping(value = "/booksjson", method = RequestMethod.GET)
+	public @ResponseBody List<Book> bookListRest() {
+		return (List<Book>) repository.findAll();
+	}
+	
+	@RequestMapping (value ="/booksjson/{id}", method = RequestMethod.GET)
+	public @ResponseBody Optional <Book> findBookRest(@PathVariable("id") Long id){
+		return repository.findById(id);
+		
+	}
+
+	// add new books to bookstore
 	@RequestMapping(value = "/newbook", method = RequestMethod.GET)
 	public String getNewBookForm(Model model) {
 		model.addAttribute("book", new Book());
@@ -43,6 +56,7 @@ public class BookController {
 		return "bookform";
 	}
 
+	// save books to bookstore
 	@RequestMapping(value = "/savebook", method = RequestMethod.POST)
 	public String saveCar(@ModelAttribute Book book) {
 		// talletetaan yhden auton tiedot tietokantaan
@@ -50,20 +64,18 @@ public class BookController {
 		return "redirect:/books";
 	}
 
+	// delete books
 	@RequestMapping(value = "/deletebook/{id}", method = RequestMethod.GET)
 	public String deleteBook(@PathVariable("id") Long bookId, Model model) {
 		repository.deleteById(bookId);
 		return "redirect:../books";
 	}
-	@RequestMapping (value = "/editbook/{id}", method = RequestMethod.POST )
-	 public String editBook (@PathVariable("id") Long bookId, Model model) {
-		 repository.findById(bookId);
-		 return "editbook";
-	 }
-	 
-	 
-	
+	// edit existing books
+	// todo make it work :)
+	@RequestMapping(value = "/editbook/{id}", method = RequestMethod.POST)
+	public String editBook(@PathVariable("id") Long bookId, Model model) {
+		repository.findById(bookId);
+		return "editbook";
 	}
-	
 
-
+}
