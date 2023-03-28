5cb68093ba5f388e0be12dbddbbc7f9cc786f737
poddui <jimi8888.podduikin@gmail.com>
2023-03-28 10:19:10 +0300

    Initial commit

---
+++ kysely/src/main/java/hh/ohjelmistoprojekti/kysely/KyselyApplication.java
@@ -0,0 +1,13 @@
+package hh.ohjelmistoprojekti.kysely;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KyselyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KyselyApplication.class, args);
+	}
+
+}
---
+++ kysely/src/test/java/hh/ohjelmistoprojekti/kysely/KyselyApplicationTests.java
@@ -0,0 +1,13 @@
+package hh.ohjelmistoprojekti.kysely;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KyselyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
